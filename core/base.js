75d3a7a48c60d1b36d4201d9d758d6c8f3a4c82f
zhang99 <zhanghuasc@qq.com>
2016-07-14 18:51:44 +0800

    Add Base.js

---
+++ core/base.js
@@ -0,0 +1,30 @@
+(function ($)
+{
+    //hwaui 继承方法
+    Function.prototype.hwaExtend = function (parent, overrides)
+    {
+        if (typeof parent != 'function') return this;
+        //保存对父类的引用
+        this.base = parent.prototype;
+        this.base.constructor = parent;
+        //继承
+        var f = function () { };
+        f.prototype = parent.prototype;
+        this.prototype = new f();
+        this.prototype.constructor = this;
+        //附加属性方法
+        if (overrides) $.extend(this.prototype, overrides);
+    };
+    //延时加载
+    Function.prototype.hwaDefer = function (o, defer, args)
+    {
+        var fn = this;
+        return setTimeout(function () { fn.apply(o, args || []); }, defer);
+    };
+
+    // 核心对象
+    window.hwa = $.ligerui = {
+        version: 'V0.0.1'
+    }
+        
+})(jQuery);
